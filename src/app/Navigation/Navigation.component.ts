8e95d3c71be9e16d36b7f444e2576e2e9f6600b3
anshumannehru <anshumannehru.steamtroops@gmail.com>
2023-12-05 21:14:10 +0530

    intial-commit

---
+++ src/app/Navigation/Navigation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-bar',
+    templateUrl:'./Navigation.component.html'
+})
+export class Navigation{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { Navigation } from './Navigation/Navigation.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { RecipeItemComponent } from './recipes-list/recipe-item/recipe-item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Navigation,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
